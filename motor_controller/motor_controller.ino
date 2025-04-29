c687ec0e00915f6b265227fcb37ae6070ae1f462
SamTech37 <SamTech37@users.noreply.github.com>
2025-04-29 11:27:44 +0800

    Fix light-to-angle mapping: adjust minimum light value and correct angle mapping logic

--- motor_controller/motor_controller.ino
+++ motor_controller/motor_controller.ino
@@ -5,7 +5,7 @@ Servo servo2;
 
 const int ldrPin = A0;  // LDR 接在 A0
 int lightValue;
-int lv_max = 1023, lv_min = 500;
+int lv_max = 1023, lv_min = 250;
 
 void setup() {
   Serial.begin(9600);
@@ -17,8 +17,10 @@ void setup() {
 void light_to_angle(){
   lightValue = analogRead(ldrPin);  // 讀取光線值（範圍：0～1023）
 
-  // 假設光越強 → 轉越大角度
-  int angle = map(lightValue, lv_max, lv_min, 0, 180);  // 映射成角度
+  // 假設光越強 → 讀到的電阻值越小，轉越大角度
+   
+  int angle = map(lightValue, lv_min, lv_max, 180, 0);  // 映射成角度
+  angle = constrain(angle, 0, 180); // clipping, ensure angle is in [0,180] 
   servo1.write(angle);
   servo2.write(180 - angle);  // 相反方向轉動
   
